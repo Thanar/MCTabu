c4b31ef3e768d7609a5d1f5f192139776155ef9b
sephi_6 <vdominguezaragon@gmail.com>
2015-01-19 19:10:43 +0100

    Tipo solucion y ruta

---
+++ MCTabu/Assets/Scripts/Ruta.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class Ruta {
+
+    public List<Cliente> recorridos = new List<Cliente>();
+    public Vehiculo tipoUtilizado;
+    public int costeRuta;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
