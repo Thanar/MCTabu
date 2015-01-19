42d32cb899ea7a2b52c35a6a9b25398b655e1bc4
sephi_6 <vdominguezaragon@gmail.com>
2015-01-19 19:45:35 +0100

    na

--- MCTabu/Assets/Scripts/TSA.cs
+++ MCTabu/Assets/Scripts/TSA.cs
@@ -3,13 +3,25 @@ using System.Collections;
 
 public class TSA : MonoBehaviour {
 
+    public ProblemaTabu problema;
 	// Use this for initialization
 	void Start () {
-	
+        problema = new ProblemaTabu(3);
+        solucionarProblema();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
+    void solucionarProblema()
+    {
+        Solucion inicial = generaSolucionInicial();
+    }
+
+    void generaSolucionInicial()
+    {
+        Vehiculo inicial = 1;
+        
+    }
 }
